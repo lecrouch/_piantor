8ee1761378bcb6c25909a8e470c0028cba756425
Julian Crouch <juliancrouch@Julians-Mac-Studio.local>
2024-06-21 08:50:03 -0700

    updates

--- kb_files/boot.py
+++ kb_files/boot.py
@@ -4,6 +4,9 @@ storage.remount("/", readonly=False)
 
 m = storage.getmount("/")
 m.label = "PIANTOR_L"
+# m.label = "PIANTOR_C_L"
+# m.label = "PIANTOR_R"
+# m.label = "PIANTOR_C_R"
 
 storage.remount("/", readonly=True)
 
